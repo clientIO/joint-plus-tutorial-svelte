075c956f5f51e4f53d3116d19de0aacbff2b19f4
James Williams <jamesgeorgewilliams@protonmail.com>
2022-05-15 10:11:46 +0200

    svelte: define ref binding for app

--- src/App.svelte
+++ src/App.svelte
@@ -1,55 +1,51 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import { dia, ui, shapes } from '@clientio/rappid';
-    import '../node_modules/@clientio/rappid/rappid.css';
+    import { onMount } from "svelte";
+    import { dia, ui, shapes } from "@clientio/rappid";
+    import "../node_modules/@clientio/rappid/rappid.css";
+
+    let ref;
 
     onMount(async () => {
-
-        const main = document.getElementById('app');
-
         const namespace = shapes;
 
         const graph = new dia.Graph({}, { cellNamespace: namespace });
 
         const paper = new dia.Paper({
-            el: document.getElementById('root'),
+            el: document.getElementById("root"),
             model: graph,
             background: {
-                color: '#F8F9FA',
+                color: "#F8F9FA",
             },
             frozen: true,
             async: true,
-            cellViewNamespace: namespace
+            cellViewNamespace: namespace,
         });
 
         const scroller = new ui.PaperScroller({
             paper,
             autoResizePaper: true,
-            cursor: 'grab'
+            cursor: "grab",
         });
 
-        main.appendChild(scroller.el);
-        scroller.render().center(); 
-
+        ref.appendChild(scroller.el);
+        scroller.render().center();
 
         const rect = new shapes.standard.Rectangle({
             position: { x: 100, y: 100 },
             size: { width: 100, height: 50 },
             attrs: {
                 label: {
-                    text: 'Hello World'
-                }
-            }
+                    text: "Hello World",
+                },
+            },
         });
-  
+
         graph.addCell(rect);
         paper.unfreeze();
     });
-
 </script>
 
-<main id="app" class="app">
-</main>
+<main bind:this={ref} class="app" />
 
 <style>
 </style>
