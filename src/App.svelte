7bfa87fe61322fcd58a746d0d6d8bfcec6174036
James Williams <jamesgeorgewilliams@protonmail.com>
2022-06-15 13:12:04 +0200

    svelte: remove redundant paper el property

--- src/App.svelte
+++ src/App.svelte
@@ -1,48 +1,47 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-    import { dia, ui, shapes } from "@clientio/rappid";
-    import "../node_modules/@clientio/rappid/rappid.css";
+  import { onMount } from 'svelte';
+  import { dia, ui, shapes } from '@clientio/rappid';
+  import '../node_modules/@clientio/rappid/rappid.css';
 
-    let ref;
+  let ref;
 
-    onMount(async () => {
-        const namespace = shapes;
+  onMount(async () => {
+    const namespace = shapes;
 
-        const graph = new dia.Graph({}, { cellNamespace: namespace });
+    const graph = new dia.Graph({}, { cellNamespace: namespace });
 
-        const paper = new dia.Paper({
-            el: document.getElementById("root"),
-            model: graph,
-            background: {
-                color: "#F8F9FA",
-            },
-            frozen: true,
-            async: true,
-            cellViewNamespace: namespace,
-        });
-
-        const scroller = new ui.PaperScroller({
-            paper,
-            autoResizePaper: true,
-            cursor: "grab",
-        });
-
-        ref.appendChild(scroller.el);
-        scroller.render().center();
-
-        const rect = new shapes.standard.Rectangle({
-            position: { x: 100, y: 100 },
-            size: { width: 100, height: 50 },
-            attrs: {
-                label: {
-                    text: "Hello World",
-                },
-            },
-        });
-
-        graph.addCell(rect);
-        paper.unfreeze();
+    const paper = new dia.Paper({
+      model: graph,
+      background: {
+        color: '#F8F9FA',
+      },
+      frozen: true,
+      async: true,
+      cellViewNamespace: namespace,
     });
+
+    const scroller = new ui.PaperScroller({
+      paper,
+      autoResizePaper: true,
+      cursor: 'grab',
+    });
+
+    ref.appendChild(scroller.el);
+    scroller.render().center();
+
+    const rect = new shapes.standard.Rectangle({
+      position: { x: 100, y: 100 },
+      size: { width: 100, height: 50 },
+      attrs: {
+        label: {
+          text: 'Hello World',
+        },
+      },
+    });
+
+    graph.addCell(rect);
+    paper.unfreeze();
+  });
 </script>
 
 <main bind:this={ref} class="app" />
